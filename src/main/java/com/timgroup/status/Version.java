7473183badda70a1734525b81b0ad78dbd545626
tanderson@7edc9554-746b-495c-92d9-cd406c1090b3 <tanderson@7edc9554-746b-495c-92d9-cd406c1090b3>
2012-04-10 17:45:06 +0000

    TomA: added some (gasp!) documentation
    --HG--
    extra : convert_revision : svn%3A7edc9554-746b-495c-92d9-cd406c1090b3/StatusPage/trunk%4099035

--- src/main/java/com/timgroup/status/Version.java
+++ src/main/java/com/timgroup/status/Version.java
@@ -1,5 +1,13 @@
 package com.timgroup.status;
 
+/**
+ * Reports a version based on the metadata for a specified anchor class. The
+ * version ultimately comes from an Implementation-Version entry in the manifest
+ * of the jar file containing the class.
+ * 
+ * <p>
+ * See {@link http://docs.oracle.com/javase/6/docs/technotes/guides/jar/jar.html}
+ */
 public class Version extends Component {
     
     private final Class<?> anchorClass;
