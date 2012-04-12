0b3681d23395aba2c288ecf0a5e48f40996fe723
tanderson@7edc9554-746b-495c-92d9-cd406c1090b3 <tanderson@7edc9554-746b-495c-92d9-cd406c1090b3>
2012-04-12 15:51:22 +0000

    TomA: refactored demo a bit
    --HG--
    extra : convert_revision : svn%3A7edc9554-746b-495c-92d9-cd406c1090b3/StatusPage/trunk%4099227

--- src/test/java/servlet/StatusPageServletDemo.java
+++ src/test/java/servlet/StatusPageServletDemo.java
@@ -12,19 +12,24 @@ public class StatusPageServletDemo {
     public static void main(String[] args) throws IOException {
         StatusPage statusPage = new StatusPage("demoApp");
         statusPage.addComponent(new VersionComponent(String.class));
-        statusPage.addComponent(new ThresholdedGaugeComponent<Integer>("timeUsed", "Amount of this minute used (sec)",
-                new Gauge<Integer>() {
-                    @Override
-                    public Integer value() {
-                        return (int) (System.currentTimeMillis() / 1000) % 60;
-                    }
-                }, 30, 50));
+        statusPage.addComponent(new ThresholdedGaugeComponent<Integer>("timeUsed", "Time used this minute (sec)", timeUsedGauge(), 30, 50));
         
         final StatusPageServlet servlet = new StatusPageServlet();
         servlet.setStatusPage(statusPage);
         
         ServletContainer container = new ServletContainer(servlet, 8888, "/status");
         container.start();
+    }
+    
+    private static Gauge<Integer> timeUsedGauge() {
+        return new Gauge<Integer>() {
+            
+            @Override
+            public Integer value() {
+                return (int) (System.currentTimeMillis() / 1000) % 60;
+            }
+            
+        };
     }
     
 }
