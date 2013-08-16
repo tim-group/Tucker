97845af6c3ccba682673798d194af194e0c97a4b
Tom Anderson <tom.anderson@timgroup.com>
2013-08-16 16:09:59 +0100

    TomA: fix throwback JUnit imports

--- src/test/java/com/timgroup/tucker/info/ReportTest.java
+++ src/test/java/com/timgroup/tucker/info/ReportTest.java
@@ -7,8 +7,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ReportTest {
     
--- src/test/java/com/timgroup/tucker/info/component/ServletVersionComponentTest.java
+++ src/test/java/com/timgroup/tucker/info/component/ServletVersionComponentTest.java
@@ -1,8 +1,8 @@
 package com.timgroup.tucker.info.component;
 
-import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
