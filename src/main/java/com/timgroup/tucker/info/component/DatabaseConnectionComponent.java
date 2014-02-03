54a8654563a5874c5d26e30a0fa01d42241bf767
Tom Denley <tom.denley@youdevise.com>
2014-02-03 09:57:37 +0000

    tomd: add initial DatabaseConnectionComponent to Tucker

---
+++ src/main/java/com/timgroup/tucker/info/component/DatabaseConnectionComponent.java
@@ -0,0 +1,49 @@
+package com.timgroup.tucker.info.component;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.timgroup.tucker.info.Component;
+import com.timgroup.tucker.info.Report;
+import com.timgroup.tucker.info.Status;
+
+/**
+ * Reports whether the application can connect to the database.
+ * 
+ */
+public final class DatabaseConnectionComponent extends Component {
+    private final ConnectionProvider connectionProvider;
+
+    public interface ConnectionProvider {
+        Connection getConnection() throws SQLException;
+    }
+
+    public DatabaseConnectionComponent(ConnectionProvider connectionProvider) {
+        this("databaseConnection", "Connection to Database", connectionProvider);
+    }
+
+    public DatabaseConnectionComponent(String id, String label, ConnectionProvider connectionProvider) {
+        super(id, label);
+        this.connectionProvider = connectionProvider;
+    }
+
+    @Override
+    public Report getReport() {
+        Connection dbConnection = null;
+        try {
+            dbConnection = connectionProvider.getConnection();
+            dbConnection.createStatement().executeQuery("select 1;");
+            return new Report(Status.OK);
+        } catch (SQLException e) {
+            return new Report(Status.CRITICAL, e.getMessage());
+        } finally {
+            if (dbConnection != null) {
+                try {
+                    dbConnection.close();
+                } catch (SQLException e) {
+                    return new Report(Status.CRITICAL, e.getMessage());
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/com/timgroup/tucker/info/component/DatabaseConnectionComponentTest.java
@@ -0,0 +1,99 @@
+package com.timgroup.tucker.info.component;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.RETURNS_MOCKS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.timgroup.tucker.info.Report;
+import com.timgroup.tucker.info.Status;
+import com.timgroup.tucker.info.component.DatabaseConnectionComponent.ConnectionProvider;
+
+public final class DatabaseConnectionComponentTest {
+
+    private final ConnectionProvider connectionProvider = mock(ConnectionProvider.class);
+
+    @Test
+    public void hasSuitableDefaultIdAndLabel() throws Exception {
+        final DatabaseConnectionComponent component = new DatabaseConnectionComponent(connectionProvider);
+        assertEquals("databaseConnection", component.getId());
+        assertEquals("Connection to Database", component.getLabel());
+    }
+
+    @Test
+    public void canBeGivenCustomisedIdAndLabel() throws Exception {
+        final DatabaseConnectionComponent component = new DatabaseConnectionComponent("a", "b", connectionProvider);
+        assertEquals("a", component.getId());
+        assertEquals("b", component.getLabel());
+    }
+
+    @Test
+    public void reportsOkIfAllIsWell() throws Exception {
+        when(connectionProvider.getConnection()).thenAnswer(RETURNS_MOCKS);
+
+        assertEquals(Status.OK, new DatabaseConnectionComponent(connectionProvider).getReport().getStatus());
+    }
+
+    @Test
+    public void reportsCriticalIfConnectionCannotBeObtained() throws Exception {
+        when(connectionProvider.getConnection()).thenThrow(new SQLException("foo"));
+        
+        final Report report = new DatabaseConnectionComponent(connectionProvider).getReport();
+        assertEquals(Status.CRITICAL, report.getStatus());
+        assertEquals("foo", report.getValue());
+    }
+
+    @Test
+    public void reportsCriticalIfStatementCannotBeObtained() throws Exception {
+        final Connection connection = mock(Connection.class);
+        when(connectionProvider.getConnection()).thenReturn(connection);
+        when(connection.createStatement()).thenThrow(new SQLException("bar"));
+
+        final Report report = new DatabaseConnectionComponent(connectionProvider).getReport();
+        assertEquals(Status.CRITICAL, report.getStatus());
+        assertEquals("bar", report.getValue());
+    }
+
+    @Test
+    public void reportsCriticalIfStatementCannotBeExecuted() throws Exception {
+        final Connection connection = mock(Connection.class);
+        when(connectionProvider.getConnection()).thenReturn(connection);
+        final Statement statement = mock(Statement.class);
+        when(connection.createStatement()).thenReturn(statement);
+        when(statement.executeQuery(Mockito.anyString())).thenThrow(new SQLException("baz"));
+
+        final Report report = new DatabaseConnectionComponent(connectionProvider).getReport();
+        assertEquals(Status.CRITICAL, report.getStatus());
+        assertEquals("baz", report.getValue());
+    }
+
+    @Test
+    public void alwaysClosesItsConnections() throws Exception {
+        final Connection connection = mock(Connection.class);
+        when(connectionProvider.getConnection()).thenReturn(connection);
+        when(connection.createStatement()).thenThrow(new SQLException("foo"));
+
+        new DatabaseConnectionComponent(connectionProvider).getReport();
+        Mockito.verify(connection).close();
+    }
+
+    @Test
+    public void reportsCriticalIfConnectionCannotBeClosed() throws Exception {
+        final Connection connection = mock(Connection.class);
+        when(connectionProvider.getConnection()).thenReturn(connection);
+        when(connection.createStatement()).thenAnswer(RETURNS_MOCKS);
+        Mockito.doThrow(new SQLException("boz")).when(connection).close();
+
+        final Report report = new DatabaseConnectionComponent(connectionProvider).getReport();
+        assertEquals(Status.CRITICAL, report.getStatus());
+        assertEquals("boz", report.getValue());
+    }
+
+}
