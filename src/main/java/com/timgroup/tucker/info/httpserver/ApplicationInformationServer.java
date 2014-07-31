936df547a6bfc5618cc523f39c2b903c602ecff1
Justin Heister <justin.heister@youdevise.com>
2014-07-31 14:17:13 +0100

    Run server with a threadpool

--- src/main/java/com/timgroup/tucker/info/httpserver/ApplicationInformationServer.java
+++ src/main/java/com/timgroup/tucker/info/httpserver/ApplicationInformationServer.java
@@ -6,12 +6,17 @@ import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.sun.net.httpserver.HttpServer;
 import com.timgroup.tucker.info.ApplicationInformationHandler;
 import com.timgroup.tucker.info.Health;
 import com.timgroup.tucker.info.Stoppable;
 import com.timgroup.tucker.info.status.StatusPageGenerator;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
 
 public class ApplicationInformationServer {
 
@@ -34,7 +39,7 @@ public class ApplicationInformationServer {
         base = constructBaseUri(port);
         server = HttpServer.create(new InetSocketAddress(port), 0);
         server.createContext(base.getPath(), new ApplicationInformationHttpHandler(handler, base));
-        server.setExecutor(null);
+        server.setExecutor(newFixedThreadPool(5, new TuckerThreadFactory()));
     }
 
     private URI constructBaseUri(int port) throws UnknownHostException, IOException {
@@ -58,4 +63,15 @@ public class ApplicationInformationServer {
         server.stop(0);
     }
 
+
+    private static class TuckerThreadFactory implements ThreadFactory {
+        final AtomicInteger threadNumber = new AtomicInteger(1);
+
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread thread = new Thread("Tucker-" + threadNumber.getAndIncrement());
+            thread.setDaemon(false);
+            return thread;
+        }
+    }
 }
