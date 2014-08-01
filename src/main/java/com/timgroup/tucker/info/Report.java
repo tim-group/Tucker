303769eb47ea0366bdde00e9d99b76345b5198e8
Tom Denley <tom.denley@timgroup.com>
2014-08-01 16:43:00 +0100

    tomd: replace tabs with spaces

--- src/main/java/com/timgroup/tucker/info/Report.java
+++ src/main/java/com/timgroup/tucker/info/Report.java
@@ -48,33 +48,33 @@ public final class Report {
         return (Throwable) value;
     }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((status == null) ? 0 : status.hashCode());
-		result = prime * result + ((value == null) ? 0 : value.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((status == null) ? 0 : status.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Report other = (Report) obj;
-		if (status != other.status)
-			return false;
-		if (value == null) {
-			if (other.value != null)
-				return false;
-		} else if (!value.equals(other.value))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Report other = (Report) obj;
+        if (status != other.status)
+            return false;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        return true;
+    }
 
     @Override
     public String toString() {
