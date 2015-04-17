1f0b422ba8c3f65571aa36c5c0ec20071445bd7e
Justin Heister <justin.heister@youdevise.com>
2015-04-17 13:15:29 +0100

    Close resultSet+statements so we stop leaking when using various CPs

--- src/main/java/com/timgroup/tucker/info/component/DatabaseConnectionComponent.java
+++ src/main/java/com/timgroup/tucker/info/component/DatabaseConnectionComponent.java
@@ -1,7 +1,9 @@
 package com.timgroup.tucker.info.component;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import com.timgroup.tucker.info.Component;
 import com.timgroup.tucker.info.Report;
@@ -25,15 +27,21 @@ public final class DatabaseConnectionComponent extends Component {
     @Override
     public Report getReport() {
         Connection dbConnection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
         try {
             dbConnection = connectionProvider.getConnection();
             final long before = System.currentTimeMillis();
-            dbConnection.createStatement().executeQuery("select 1;");
+            statement = dbConnection.createStatement();
+            resultSet = statement.executeQuery("select 1;");
             final long after = System.currentTimeMillis();
             return new Report(Status.OK, (after - before) + "ms" );
         } catch (SQLException e) {
             return new Report(Status.CRITICAL, e.getMessage());
         } finally {
+            if (resultSet != null) { try { resultSet.close(); } catch (SQLException e) {}  }
+            if (statement != null) { try { statement.close(); } catch (SQLException e) {} }
+
             if (dbConnection != null) {
                 try {
                     dbConnection.close();
