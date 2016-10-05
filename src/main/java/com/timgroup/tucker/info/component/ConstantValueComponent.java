6544b4348301979208d2fddd5893c111a95b1b2d
Graham Allan <graham.allan@timgroup.com>
2016-10-05 22:51:31 +0100

    Add a component that contains a fixed value, as concisely as possible.

---
+++ src/main/java/com/timgroup/tucker/info/component/ConstantValueComponent.java
@@ -0,0 +1,25 @@
+package com.timgroup.tucker.info.component;
+
+import com.timgroup.tucker.info.Component;
+import com.timgroup.tucker.info.Report;
+import com.timgroup.tucker.info.Status;
+
+public class ConstantValueComponent extends Component {
+
+    private final Report report;
+
+    public ConstantValueComponent(String id, String label, Status status, Object value) {
+        this(id, label, new Report(status, value));
+    }
+
+    public ConstantValueComponent(String id, String label, Report report) {
+        super(id, label);
+        this.report = report;
+    }
+
+    @Override
+    public Report getReport() {
+        return report;
+    }
+
+}
