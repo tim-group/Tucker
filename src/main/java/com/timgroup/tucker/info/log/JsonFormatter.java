0e37ab9d7db1531c99be619a260aa91546c32748
Steve Haslam <steve.haslam@timgroup.com>
2019-05-09 12:04:51 +0100

    Widen type bound

--- src/main/java/com/timgroup/tucker/info/log/JsonFormatter.java
+++ src/main/java/com/timgroup/tucker/info/log/JsonFormatter.java
@@ -1,17 +1,17 @@
 package com.timgroup.tucker.info.log;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.function.Consumer;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonGenerator;
-
 public final class JsonFormatter {
     private static final JsonFactory JSON_FACTORY = new JsonFactory().enable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
 
-    public static JsonGenerator generate(Consumer<String> sink) throws IOException {
+    public static JsonGenerator generate(Consumer<? super String> sink) throws IOException {
         StringWriter sw = new StringWriter();
         FilterWriter fw = new FilterWriter(sw) {
             @Override
