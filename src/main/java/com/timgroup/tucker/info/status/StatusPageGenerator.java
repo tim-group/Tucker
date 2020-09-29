9ff09c0f7412143fd7e56ff516f5a96f16e871a3
Waseem Taj <waseem.taj@timgroup.com>
2020-09-29 18:28:47 +0100

    wtaj - removed TODO that I added which are now resolved after a chat with Max.

--- src/main/java/com/timgroup/tucker/info/status/StatusPageGenerator.java
+++ src/main/java/com/timgroup/tucker/info/status/StatusPageGenerator.java
@@ -30,8 +30,7 @@ public class StatusPageGenerator {
 
     private static final Gauge STATUS_GAUGE = Gauge.build("tucker_component_status", "Tucker Status Page Component Status converted to metrics.")
            .labelNames(StatusPage.METRIC_LABELS.toArray(new String[]{}))
-            .register();   //TODO: Waz - chat with Max: Registering to default is risky if the app constructs Metrics with non defaultRegistry. Perhaps StatusPageGenerator should take in Metrics object and use metrics.getCollectorRegistry() so it matches the registry used in  MetricsWriter
-
+            .register();
 
     private final String applicationId;
     private final VersionComponent versionComponent;
@@ -101,7 +100,7 @@ public class StatusPageGenerator {
         for(Status status : Status.values()) {
             STATUS_GAUGE.setChild(new Gauge.Child() {
                 @Override public double get() {
-                    Report report = component.getReport();  //TODO: waz - chat with max on this. We will be calling this multiple times  (once per status) - hoping its okay as slow ones should be async components.
+                    Report report = component.getReport();
                     return report.getStatus() == status ? 1 : 0;
                 }
             }, component.getId(), status.name().toLowerCase());
