d1728e93f824fababf2a8b81474c344a8a0096fe
Steve Haslam <steve.haslam@iongroup.com>
2025-11-28 12:05:39 +0000

    Support setting artifactId on extension rather than using extra properties

--- buildSrc/src/main/groovy/com/timgroup/build/TimRepoExtension.groovy
+++ buildSrc/src/main/groovy/com/timgroup/build/TimRepoExtension.groovy
@@ -1,7 +1,11 @@
 package com.timgroup.build
 
+import org.gradle.api.Project
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.Property
 import org.gradle.api.provider.Provider
 import org.gradle.api.provider.ProviderFactory
+import org.gradle.api.tasks.Input
 
 import javax.inject.Inject
 
@@ -11,9 +15,10 @@ class TimRepoExtension {
     final Provider<String> nexusRepoPassword
     final Provider<String> codeartifactUrl
     final Provider<String> codeartifactToken
+    final Property<String> artifactId
 
     @Inject
-    TimRepoExtension(ProviderFactory providers) {
+    TimRepoExtension(ObjectFactory objectFactory, ProviderFactory providers, Project project) {
         nexusRepoUrl = providers.gradleProperty("repoUrl")
         nexusRepoUsername = providers.gradleProperty("repoUsername")
         nexusRepoPassword = providers.gradleProperty("repoPassword")
@@ -22,5 +27,12 @@ class TimRepoExtension {
                 .orElse("https://timgroup-148217964156.d.codeartifact.eu-west-1.amazonaws.com/maven/jars/")
         codeartifactToken = providers.environmentVariable("CODEARTIFACT_TOKEN")
                 .orElse(providers.gradleProperty("codeartifact.token"))
+        artifactId = objectFactory.property(String).convention(providers.provider {
+            def rootProject = project.rootProject
+            if (project == rootProject)
+                return project.name
+            else
+                return "${rootProject.name}-${project.name}"
+        })
     }
 }
